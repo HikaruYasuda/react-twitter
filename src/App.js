3d808e85d53173b7ae66b20b2997b7b543b03169
HikaruYasuda <yasuda.hikaru@gmail.com>
2019-05-07 18:12:07 +0900

    lesson

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Item from './Item'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    time: new Date().toLocaleString(),
+    items: [
+      'a',
+      'b',
+      'c',
+    ]
+  }
+
+  componentDidMount() {
+    // アローFunction
+    // 関数が定義されているブロックのthisが使える
+    setInterval(() => {
+      this.setState({
+        time: new Date().toLocaleString()
+      })
+    }, 1000)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React<br />
+            { this.state.time }<br/>
+            { this.state.items.map(item => {
+              return <Item message={item}/>
+            }) }
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
