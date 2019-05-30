734b87b8a668d5a5991709b8bc73bf1f67615d1f
HikaruYasuda <yasuda.hikaru@gmail.com>
2019-05-30 12:42:09 +0900

    [Lesson] Reduxの追加

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg'
 import React from 'react'
+import { connect } from 'react-redux'
 
-export default function Header() {
+function Header(props) {
   return (
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
+      <div>{ props.user_name }</div>
     </header>
   )
 }
+
+const mapStateToProps = state => {
+  return {
+    user_name: state.user_name,
+  }
+}
+export default connect(mapStateToProps)(Header)
